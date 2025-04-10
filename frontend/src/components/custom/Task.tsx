cf87271dd2371095eb53d562a5d13b6ee8114695
Roman <jromanbrugno@gmail.com>
2025-04-10 10:10:16 -0300

    Add task component.

---
+++ frontend/src/components/custom/Task.tsx
@@ -0,0 +1,25 @@
+import { Task } from "types/dataTypes"
+import { Badge } from "../ui/badge"
+import { Trash2 } from "lucide-react"
+import { motion } from "motion/react"
+interface props {
+    task: Task
+}
+
+function TaskCard({ task }: props) {
+    return (
+        <motion.div 
+            initial={{scale:0}}
+            animate={{scale:1}}
+            className="bg-appLight h-32 rounded-lg py-2 px-3"
+        >
+            <Badge className="bg-appPrimary">Diseño</Badge>
+            <div className="flex justify-between items-center">
+                <h2>{task.name}</h2>
+                <button className="bg-appPrimary p-1 rounded-md text-appLight"><Trash2 /></button>
+            </div>
+        </motion.div>
+    )
+}
+
+export default TaskCard
