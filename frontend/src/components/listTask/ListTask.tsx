3caa6cec95ba2fc8698dbe729b703f5ee64e2f77
Roman <jromanbrugno@gmail.com>
2025-04-21 13:05:45 -0300

    refactor AddTaskDialog component.

--- frontend/src/components/listTask/ListTask.tsx
+++ frontend/src/components/listTask/ListTask.tsx
@@ -2,7 +2,7 @@ import { ListType } from '@/types/dataTypes'
 
 //Componets
 import Task from '@/components/task/Task'
-import AddTask from '@/components/task/AddTaskDialog'
+import AddTaskDialog from '@/components/task/AddTaskDialog'
 //Hooks
 import useTasks from '@/hooks/useTasks'
 import useLists from '@/hooks/useLists'
@@ -14,6 +14,7 @@ import { Plus, Trash2 } from 'lucide-react'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import SkeletonTask from '../task/SkeletonTask'
+import { useState } from 'react'
 
 interface props {
   list: ListType
@@ -24,6 +25,7 @@ function ListTask({ list }: props) {
   const { deleteList } = useLists(id ? parseInt(id) : undefined)
   const { tasks, isLoading, isError, deleteTask } = useTasks(list.id)
 
+  const [openModal, setOpenModal] = useState(false)
   return (
     <>
       <Card className="min-w-96 h-full font-app ">
@@ -33,17 +35,20 @@ function ListTask({ list }: props) {
             <Badge className="bg-appPrimary">{list.tasks.length}</Badge>
           </div>
           <div className="flex gap-2">
-            <AddTask listId={list.id}>
-              <button className="p-1 rounded-md text-gray-500 transition-colors hover:bg-appPrimary hover:text-appLight">
-                <Plus />
-              </button>
-            </AddTask>
+            <button
+              onClick={() => setOpenModal(true)}
+              className="p-1 rounded-md text-gray-500 transition-colors hover:bg-appPrimary hover:text-appLight"
+            >
+              <Plus />
+            </button>
             <button
               className="p-1 rounded-md  text-gray-500 transition-colors hover:bg-appPrimary hover:text-appLight"
               onClick={() => deleteList(list.id)}
             >
               <Trash2 size={20} />
             </button>
+
+            <AddTaskDialog listId={list.id} open={openModal} onClose={() => setOpenModal(false)} />
           </div>
         </CardHeader>
         <CardContent className="h-full flex flex-col gap-2 pt-2 overflow-y-scroll">
--- frontend/src/components/task/AddTaskDialog.tsx
+++ frontend/src/components/task/AddTaskDialog.tsx
@@ -1,37 +1,33 @@
-import { ReactNode, useState } from 'react'
-//Shadcn
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-  DialogDescription,
-} from '@/components/ui/dialog'
 //React-hook-form
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 import useTasks from '@/hooks/useTasks'
 import { TaskRequestType } from '@/types/dataTypes'
-interface props {
-  listId: number
-  children: ReactNode
-}
 
+interface AddTaskDialog {
+  listId: number
+  open: boolean
+  onClose: () => void
+}
 type TaskForm = {
   name: string
   type: 'Design' | 'Development' | 'Testing' | 'Deployment'
   description: string
 }
-
-function AddTask({ listId, children }: props) {
+function AddTaskDialog({ listId, open, onClose }: AddTaskDialog) {
   const { createTask } = useTasks(listId)
-  const [open, setOpen] = useState(false)
   const {
     register,
     formState: { errors },
     reset,
     handleSubmit,
   } = useForm<TaskForm>()
+
+  const onCancel = () => {
+    reset()
+    onClose()
+  }
+
   const onSubmit: SubmitHandler<TaskForm> = data => {
     const task: TaskRequestType = {
       name: data.name,
@@ -41,105 +37,108 @@ function AddTask({ listId, children }: props) {
       listId: listId,
     }
     createTask(task)
-    setOpen(false)
+    onClose()
     reset()
   }
 
   const taskType = ['Design', 'Development', 'Testing', 'Deployment']
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent
-        className="text-white bg-background-dark border-background-dark"
-        onInteractOutside={() => reset()}
-        onCloseAutoFocus={() => reset()}
+    // Overlay
+    <div
+      className={`fixed inset-0 m-0 w-screen h-screen z-100 flex justify-center items-center transition-colors ${open ? 'visible bg-black/50' : 'invisible'}`}
+      onClick={onCancel}
+    >
+      {/* Content */}
+      <div
+        className={`w-xl h-fit mx-4 flex flex-col gap-4 bg-background-dark z-150 rounded-sm shadow p-6 transition-all ${open ? 'scale-100 opacity-100' : 'scale-105 opacity-0'}`}
+        onClick={e => e.stopPropagation()}
       >
-        <DialogTitle>Add new Task</DialogTitle>
-        <DialogDescription asChild>
-          <form onSubmit={handleSubmit(onSubmit)} className="text-white flex flex-col gap-2">
-            <div>
-              <label htmlFor="name" className="block mb-1">
-                Task name
-              </label>
-              <div className="flex justify-start items-center gap-2">
-                <input
-                  {...register('name', {
-                    maxLength: 20,
-                    min: 3,
-                    pattern: /^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\s]+$/,
-                    required: true,
-                  })}
-                  id="name"
-                  placeholder="Name"
-                  className="w-48 h-8 text-black bg-appLight rounded-xs p-2 outline-none placeholder:text-gray-700"
-                />
-
-                {errors.name?.type === 'required' && (
-                  <p role="alert" className="text-red-500">
-                    Name es required
-                  </p>
-                )}
-                {errors.name?.type === 'maxLength' && (
-                  <p role="alert" className="text-red-500">
-                    Max length is 20 characters
-                  </p>
-                )}
-                {errors.name?.type === 'pattern' && (
-                  <p role="alert" className="text-red-500">
-                    Only alphanumeric characters
-                  </p>
-                )}
-              </div>
-            </div>
-            <div>
-              <label htmlFor="typeTask" className="block mb-1">
-                Type task
-              </label>
-              <select
-                defaultValue={'Desing'}
-                {...register('type', {
-                  required: true,
-                })}
-                className="w-48 h-8 text-black bg-appLight rounded-xs pl-2 outline-none placeholder:text-gray-700"
-              >
-                {taskType.map(type => (
-                  <option key={type} className="text-black bg-appLight" value={type}>
-                    {type}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label htmlFor="description" className="block mb-1">
-                Description
-              </label>
-              <textarea
-                {...register('description', {
-                  maxLength: 250,
-                  required: true,
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="text-white font-app font-normal flex flex-col gap-2"
+        >
+          <div>
+            <label htmlFor="name" className="block mb-1 ">
+              Task name
+            </label>
+            <div className="flex justify-start items-center gap-2">
+              <input
+                {...register('name', {
+                  maxLength: 20,
+                  min: 3,
                   pattern: /^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\s]+$/,
+                  required: true,
                 })}
-                id="description"
-                placeholder="Description"
-                className="w-full h-32 resize-none text-black bg-appLight rounded-xs p-2 outline-none placeholder:text-gray-700"
+                id="name"
+                placeholder="Name"
+                className="w-48 h-8 text-white bg-appLight rounded-xs p-2 outline-none placeholder:text-gray-700"
               />
-              {errors.description?.type === 'required' && (
+
+              {errors.name?.type === 'required' && (
                 <p role="alert" className="text-red-500">
-                  Description is required
+                  Name es required
                 </p>
               )}
-              {errors.description?.type === 'pattern' && (
+              {errors.name?.type === 'maxLength' && (
+                <p role="alert" className="text-red-500">
+                  Max length is 20 characters
+                </p>
+              )}
+              {errors.name?.type === 'pattern' && (
                 <p role="alert" className="text-red-500">
                   Only alphanumeric characters
                 </p>
               )}
             </div>
-            <button className="bg-app">Submit</button>
-          </form>
-        </DialogDescription>
-      </DialogContent>
-    </Dialog>
+          </div>
+          <div>
+            <label htmlFor="typeTask" className="block mb-1">
+              Type task
+            </label>
+            <select
+              defaultValue={'Desing'}
+              {...register('type', {
+                required: true,
+              })}
+              className="w-48 h-8 text-white bg-appLight rounded-xs pl-2 outline-none placeholder:text-gray-700"
+            >
+              {taskType.map(type => (
+                <option key={type} className="text-white bg-appLight" value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="description" className="block mb-1">
+              Description
+            </label>
+            <textarea
+              {...register('description', {
+                maxLength: 250,
+                required: true,
+                pattern: /^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\s]+$/,
+              })}
+              id="description"
+              placeholder="Description"
+              className="w-full h-32 resize-none text-white bg-appLight rounded-xs p-2 outline-none placeholder:text-gray-700"
+            />
+            {errors.description?.type === 'required' && (
+              <p role="alert" className="text-red-500">
+                Description is required
+              </p>
+            )}
+            {errors.description?.type === 'pattern' && (
+              <p role="alert" className="text-red-500">
+                Only alphanumeric characters
+              </p>
+            )}
+          </div>
+          <button className="bg-app">Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default AddTask
+export default AddTaskDialog
