1ea53e3b0a77f29e35a2d778960ab559834bfb84
Roman <jromanbrugno@gmail.com>
2025-04-21 15:02:32 -0300

    restyling confirm dialog.

--- frontend/src/components/dialog/ConfirmDialog.tsx
+++ frontend/src/components/dialog/ConfirmDialog.tsx
@@ -17,16 +17,22 @@ function ConfirmDialog({ title, description, open, onClose, onAction }: ConfirmD
       onClick={onClose}
     >
       <div
-        className={`flex flex-col gap-4 bg-appPrimary z-150 rounded-sm shadow p-6 transition-all max-w-md ${open ? 'scale-100 opacity-100' : 'scale-105 opacity-0'}`}
+        className={`flex flex-col gap-4 bg-background-dark z-150 rounded-sm shadow p-6 transition-all max-w-md ${open ? 'scale-100 opacity-100' : 'scale-105 opacity-0'}`}
         onClick={e => e.stopPropagation()}
       >
-        <h2 className="font-bold">{title}</h2>
+        <h2 className="font-bold text-white">{title}</h2>
         {description && <p>{description}</p>}
         <div className="flex justify-between">
-          <button className="bg-red-500 px-2 py-1 rounded-sm" onClick={onClose}>
+          <button
+            className="hover:bg-red-500 transition-colors px-2 py-1 rounded-sm"
+            onClick={onClose}
+          >
             Cancel
           </button>
-          <button className="bg-green-500 px-2 py-1 rounded-sm" onClick={onConfirm}>
+          <button
+            className="hover:bg-green-500 transition-colors px-2 py-1 rounded-sm"
+            onClick={onConfirm}
+          >
             Confirm
           </button>
         </div>
