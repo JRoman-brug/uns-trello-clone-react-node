a4d1b4c08ee605d86ae20ea851b19977ace011bc
Roman <jromanbrugno@gmail.com>
2025-04-23 23:57:35 -0300

    Implement task dialog.

--- frontend/src/components/task/Task.tsx
+++ frontend/src/components/task/Task.tsx
@@ -9,6 +9,7 @@ import { motion } from 'motion/react'
 import ConfirmDialog from '@/components/dialog/ConfirmDialog'
 import { useState } from 'react'
 import EditTaskDialog from './EditTaskDialog'
+import TaskDialog from './TaskDialog'
 
 interface props {
   task: TaskType
@@ -16,8 +17,9 @@ interface props {
 }
 
 function Task({ task, onClick }: props) {
-  const [openModal, setOpenModel] = useState(false)
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false)
   const [openEditTaskDialog, setOpenEditTaskDialog] = useState(false)
+  const [openTaskDialog, setOpenTaskDialog] = useState(false)
   return (
     <motion.div
       initial={{ scale: 0 }}
@@ -25,13 +27,17 @@ function Task({ task, onClick }: props) {
       exit={{ scale: 0 }}
       transition={{ delay: 0.5 }}
       className="bg-appLight shrink-0 h-32 rounded-lg py-2 px-3"
+      onClick={() => setOpenTaskDialog(true)}
     >
       <Badge className="bg-appPrimary">Diseño</Badge>
       <div className="flex justify-between items-center">
         <h3>{task.name}</h3>
         <div className="flex">
           <button
-            onClick={() => setOpenEditTaskDialog(true)}
+            onClick={e => {
+              e.stopPropagation()
+              setOpenEditTaskDialog(true)
+            }}
             className="p-1 rounded-md text-gray-500 transition-colors hover:bg-appPrimary hover:text-appLight"
           >
             <Pen size={20} />
@@ -45,18 +51,22 @@ function Task({ task, onClick }: props) {
 
           <button
             className="p-1 rounded-md text-gray-500 transition-colors hover:bg-appPrimary hover:text-appLight"
-            onClick={() => setOpenModel(true)}
+            onClick={e => {
+              e.stopPropagation()
+              setOpenConfirmDialog(true)
+            }}
           >
             <Trash2 size={20} />
           </button>
           <ConfirmDialog
             title="Are you sure you want to delete this task?"
-            open={openModal}
-            onClose={() => setOpenModel(false)}
+            open={openConfirmDialog}
+            onClose={() => setOpenConfirmDialog(false)}
             onAction={onClick}
           />
         </div>
       </div>
+      <TaskDialog task={task} open={openTaskDialog} onClose={() => setOpenTaskDialog(false)} />
     </motion.div>
   )
 }
---
+++ frontend/src/components/task/TaskDialog.tsx
@@ -0,0 +1,46 @@
+import { TaskType } from '@/types/dataTypes'
+import { Badge } from '@/components/ui/badge'
+import { Check, X } from 'lucide-react'
+
+interface TaskDialog {
+  task: TaskType
+  open: boolean
+  onClose: () => void
+}
+function TaskDialog({ task, open, onClose }: TaskDialog) {
+  const onCancel = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    console.log('cancel')
+    onClose()
+  }
+
+  return (
+    // Overlay
+    <div
+      className={`fixed inset-0 m-0 w-screen h-screen z-100 flex justify-center items-center transition-colors ${open ? 'visible bg-[#0008]' : 'invisible'}`}
+      onClick={onCancel}
+    >
+      {/* Content */}
+      <div
+        className={`w-xl h-fit mx-4 flex flex-col gap-4 bg-background-dark z-150 rounded-sm shadow p-6 transition-all ${open ? 'scale-100 opacity-100' : 'scale-105 opacity-0'}`}
+        onClick={(e: React.MouseEvent) => e.stopPropagation()}
+      >
+        <h2 className="text-2xl font-bold text-white">{task.name}</h2>
+        <Badge className="bg-white text-black">{task.type}</Badge>
+        <p className="text-white">{task.description}</p>
+        <div className="flex gap-2 items-center text-white">
+          <p>Complete:</p>
+          {task.isCompleted ? <Check /> : <X />}
+        </div>
+        <button
+          className="bg-[#2d3338] hover:bg-[#48515a] text-white transition-colors px-2 py-1 rounded-sm"
+          onClick={onCancel}
+        >
+          exit
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default TaskDialog
