1d8d2598a33cf2f832a6459088b2fe1b1374ff8c
Franco Popp <franco.a.popp9@gmail.com>
2025-05-08 15:12:31 -0300

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    host: true
-  }
+    host: 'react-express-popp-brugnoni.onrender.com',
+  },
 })
